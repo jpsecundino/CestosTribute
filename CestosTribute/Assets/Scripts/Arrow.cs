e93ba19c5576d28bc5bf0df0ae38bfe622582b0d
giovanidl <giovanilucafo@usp.br>
2020-07-24 00:40:32 -0300

    Funcionando a colisão e movimentação das bolas, ainda não funcionando perfeitamente as setas de direção e força.

---
+++ CestosTribute/Assets/Scripts/Arrow.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+        
+    //This doesn’t need to be public, but it helps when you’re initially trying to find the right size
+    public float arrowheadSize;
+    public Vector3 startPosition, endPosition, ballPosition;
+    GameObject arrow;
+    LineRenderer arrowLine;
+
+    void Start(){
+        arrowLine = GetComponent<LineRenderer>();
+        arrowheadSize = 0.02f;
+    }
+    private void Update() {
+        startPosition = transform.position;
+    }
+
+    void OnMouseDrag(){
+        
+        //Turn on the arrow
+        arrowLine.enabled = true;
+        DrawArrow ();
+
+    }
+
+    void DrawArrow(){
+
+        endPosition = Camera.main.ScreenToWorldPoint (
+        new Vector3 (Input.mousePosition.x,
+        Input.mousePosition.y,
+        Camera.main.nearClipPlane
+        ));
+
+        //The longer the line gets, the smaller relative to the entire line the arrowhead should be
+        float percentSize = (float) (arrowheadSize / Vector3.Distance (startPosition, endPosition));
+        //h/t ShawnFeatherly (http://answers.unity.com/answers/1330338/view.html)
+        arrowLine.SetPosition (0, startPosition);
+        arrowLine.SetPosition (1, Vector3.Lerp(startPosition, endPosition,1));
+        arrowLine.SetPosition (2, Vector3.Lerp (startPosition, endPosition,1));
+        arrowLine.SetPosition (3, endPosition);
+        arrowLine.widthCurve = new AnimationCurve (
+        new Keyframe (0, 0.4f),
+        new Keyframe (0.999f - percentSize, 0.4f),
+        new Keyframe (1 - percentSize, 1f),
+        new Keyframe (1 - percentSize, 1f),
+        new Keyframe (1, 0f));
+
+    }
+
+    void OnMouseUp(){
+        //Turn off the arrow
+        arrowLine.enabled = false;
+
+    }
+
+}
---
+++ CestosTribute/Assets/Scripts/DragAndThrow.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DragAndThrow : MonoBehaviour
+{
+    Vector3 startPos, endPos, direction;
+
+    public GameObject movedObject;
+    public bool validClick = false;
+    public float radius = 0.52f;
+
+    public float throwForce = 10000;
+
+    [Range (0f,1f)]
+    public float multiplier = 0.5f;
+
+    //a força depende do tamanho do tamanho do vetor direcao
+                //limitar o tamanho do vetor direcao
+    void Update(){
+        if (Input.GetMouseButtonDown(0) && ClickOnBall(Camera.main.ScreenToWorldPoint(Input.mousePosition))) {
+            startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            validClick = true;
+        }
+        if (Input.GetMouseButtonUp(0) && validClick) {
+            validClick = false;                              
+            endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            direction = startPos - endPos;
+            GetComponent<Rigidbody2D>().AddForce(-direction * throwForce * multiplier);
+        }
+    }
+
+    private bool ClickOnBall(Vector3 mousePosition){
+        Vector2 objPos = new Vector2(movedObject.transform.position.x, movedObject.transform.position.y);
+        Vector2 mousePos = new Vector2(mousePosition.x, mousePosition.y);
+        
+        float distance = Vector2.Distance(objPos, mousePos);
+        
+        //Debug.Log("Distancia: " + distance + " mouse: " + mousePosition + " objeto: "+ movedObject.transform.position);
+        
+        return distance < radius;
+    }
+}
