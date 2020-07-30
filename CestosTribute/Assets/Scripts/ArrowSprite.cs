95d35ae14c214ff0e9b813b7117fa3cae6b4bade
jpsecundino <jpsecundino@users.noreply.github.com>
2020-07-30 04:32:18 -0300

    arrows working!

--- CestosTribute/Assets/Scripts/ArrowSprite.cs
+++ CestosTribute/Assets/Scripts/ArrowSprite.cs
@@ -11,12 +11,17 @@ public class ArrowSprite : MonoBehaviour
     private Ball ballScript;
     private bool validClick = false;
     private Renderer arrowSprite;
+    private Vector3 initialScale;
+
+    public float maxSize = 2f;
+    public float minSize = 0.5f;
 
     void Start()
     {
         ballScript = ball.GetComponent<Ball>();      
         arrowSprite = GetComponent<Renderer>();
         arrowSprite.enabled = false;
+        initialScale = transform.localScale;
     }
     
     // Update is called once per frame
@@ -26,46 +31,52 @@ public class ArrowSprite : MonoBehaviour
         {
             validClick = true;
             arrowSprite.enabled = true;
-            startPoint = ball.transform.position;
-            transform.position = startPoint;
-
         }
         if (Input.GetMouseButton(0) && validClick)
         {
+            startPoint = ball.transform.position;
+            transform.position = startPoint;
             endPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            
-            Vector2 dir = new Vector2( endPoint.x, endPoint.y ) - new Vector2(startPoint.x, startPoint.y);
-           
-            float angle = Mathf.Atan2(dir.y + startPoint.y, dir.x + startPoint.x) * Mathf.Rad2Deg;
-            
-            Debug.Log(dir);
-            
-
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            ClipDistance(endPoint, startPoint);
+            ChangeAngle();
+            ChangeArrowSize();
         }
+        
         if (Input.GetMouseButtonUp(0) && validClick)
         {
             arrowSprite.enabled = false;
             validClick = false;
         }
     }
+    
+    private void ChangeArrowSize(){
+        float distance = Vector2.Distance(startPoint, endPoint);
+        transform.localScale = initialScale * distance;
+    }
 
-    // private void  allowedDragDistance(Vector3 mousePosition, Vector3 startPos){
+    private void ChangeAngle(){
+        Vector2 dir = new Vector2( endPoint.x, endPoint.y ) - new Vector2(startPoint.x, startPoint.y);
+        float angle = Vector2.SignedAngle(dir, new Vector2(1,0));
+        transform.rotation = Quaternion.AngleAxis(- angle, Vector3.forward);
+    }
+    
+    private void  ClipDistance(Vector3 mousePosition, Vector3 startPos){
         
-    //     Vector2 objPos = new Vector2(startPos.x, startPos.y);
-    //     Vector2 mousePos = new Vector2(mousePosition.x, mousePosition.y);
-    //     Vector2 direction = (mousePos - objPos).normalized;
+        Vector2 objPos = new Vector2(startPos.x, startPos.y);
+        Vector2 mousePos = new Vector2(mousePosition.x, mousePosition.y);
+        Vector2 direction = (mousePos - objPos).normalized;
 
-    //     float distance = Vector2.Distance(objPos, mousePos);
+        float distance = Vector2.Distance(objPos, mousePos);
 
-    //     if (distance <= maxSize && distance >= minSize){
-    //         endPos = mousePosition + camOffset;
-    //     }else{
-    //         if(distance > maxSize){
-    //             endPos = ((Vector3)direction * maxSize) + startPos;
-    //         }else{
-    //             endPos = startPos;
-    //         }
-    //     }
-    // }
+        if (distance <= maxSize && distance >= minSize){
+            endPoint = mousePosition;
+        }else{
+            if(distance > maxSize){
+                endPoint = ((Vector3)direction * maxSize) + startPos;
+            }else{
+                endPoint = startPos;
+            }
+        }
+
+    }
 }
