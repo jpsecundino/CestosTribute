b44d32e5eba56c7478920e556e0c374ec6420758
jpsecundino <jpsecundino@users.noreply.github.com>
2020-07-31 04:13:36 -0300

    background + border collider

---
+++ CestosTribute/Assets/Scripts/Border.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Border : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("entrei");    
+        if(other.CompareTag("Sphere")){
+            other.gameObject.GetComponent<Effects>().Dissolve();
+            other.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0,0,0);
+        }
+    }
+}
