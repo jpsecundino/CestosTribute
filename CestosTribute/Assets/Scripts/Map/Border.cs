2c8308a73ca4d599008f00a7b18ddd4c5a826ad8
jpsecundino <jpsecundino@users.noreply.github.com>
2020-08-02 19:46:31 -0300

    added filter to ball sprites

--- CestosTribute/Assets/Scripts/Map/Border.cs
+++ CestosTribute/Assets/Scripts/Map/Border.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Border : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D other)
     {   
         if(other.CompareTag("Sphere")){
             other.gameObject.GetComponent<Ball>().DestroyBall();
