a78ddb7a7de9d2e75dc9760f7ad7286845e178db
jpsecundino <jpsecundino@users.noreply.github.com>
2020-08-17 00:37:16 -0300

    rematch scene logic and ui

---
+++ CestosTribute/Assets/Scripts/RematchScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RematchScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] trophyPositions;
+    public GameObject trophy;
+    public int playerId;
+
+    void Update(){
+        PositionTrophy(playerId);
+    }
+
+    public void PositionTrophy(int _playerId){
+        trophy.transform.position = trophyPositions[_playerId -1].transform.position;
+    }
+
+}
